3308d4f483d9239df5ac8e3788df9fea6c628e79
brunoalvesdasilvabr <brunoalvesdasilvabr@gmail.com>
2018-12-30 07:45:55 -0200

    dado rolando

---
+++ js/index.js
@@ -0,0 +1,21 @@
+var  scores, roundScore, activePlayer;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+
+
+// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+document.querySelector('.dice').style.display = 'none';
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+
+   var dice =  Math.floor(Math.random() *6) + 1;
+
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
+});
