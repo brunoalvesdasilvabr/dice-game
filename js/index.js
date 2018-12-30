ad03e03574e62b6f8c7c698070dbb77369680ffb
brunoalvesdasilvabr <brunoalvesdasilvabr@gmail.com>
2018-12-30 20:59:56 -0200

    score

--- js/index.js
+++ js/index.js
@@ -4,9 +4,13 @@ scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
 
-// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
 
 document.querySelector('.dice').style.display = 'none';
 
@@ -18,4 +22,17 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
     diceDOM.style.display = 'block';
     diceDOM.src = 'dice-' + dice + '.png';
 
+    if (dice !== 1){
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    }else{
+    
+    }
+
+
 });
+
+
+
+
+// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
